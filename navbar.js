dae5e79f0b03ab5391c18dd893ab889d12a168d2
Nhyoochan Rajkarnikar <nhyoochan1309@gmail.com>
2026-09-23 19:22:23 +0545

    new mail

--- navbar.js
+++ navbar.js
@@ -212,7 +212,7 @@
       </a>
 
       <!-- Mail -->
-      <a href="mailto:nhyoochan1309@gmail.com" class="header-icon" style="background: #EA4335;" aria-label="Email">
+      <a href="mailto:nhyoochan@nhyoochan.com.np" class="header-icon" style="background: #EA4335;" aria-label="Email">
         <svg viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
       </a>
     </div>
